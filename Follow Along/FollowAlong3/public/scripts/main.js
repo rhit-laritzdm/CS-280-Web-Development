83e11ca5dab28afab9d2db5c7f133eb3723f0d57
laritzdm <laritzdm@rose-hulman.edu>
2023-09-14 14:17:20 -0400

    Finished Follow Along 3

--- Follow Along/FollowAlong3/public/scripts/main.js
+++ Follow Along/FollowAlong3/public/scripts/main.js
@@ -1,3 +1,26 @@
+let counter = 0;
+
 main = function() {
     console.log("Ready");
+    document.querySelector("#decrementButton").onclick = (event) => {
+        console.log("decrement button");
+        counter = counter - 1;
+        updateView();
+    };
+    document.querySelector("#resetButton").onclick = (event) => {
+        console.log("reset button");
+        counter = 0;
+        updateView();
+    };
+    document.querySelector("#incrementButton").onclick = (event) => {
+        console.log("increment button");
+        counter = counter + 1;
+        updateView();
+    };
 }
+
+updateView = function() {
+    document.querySelector('#counterText').innerHTML = `Count = ${counter}`
+}
+
+main();
