e7a1471562015688398ed279e39c846b9c289126
laritzdm <laritzdm@rose-hulman.edu>
2023-10-01 15:24:52 -0400

    Working on step 8

--- Follow Along/FollowAlong5/scripts/main.js
+++ Follow Along/FollowAlong5/scripts/main.js
@@ -18,13 +18,38 @@ function selectWinnerBoxes(winnerBoxes) {
 
 // Function to check and get the winner
 function getWinner() {
+    const winningCombinations = [
+        [0,1,2], [3,4,5], [6,7,8], // Rows
+        [0,3,6], [1,4,7], [2,5,8], // Columns
+        [0,4,8], [2,4,6]           // Diagonals
+    ];
 
+    for (const combination of winningCombinations) {
+        const [a,b,c] = combination;
+        const boxA = boxes[a], boxB = boxes[b], boxC = boxes[c];
+        console.log(a,b,c);
+        console.log(boxA,boxB,boxC);
+    }
 };
 
 // Function to handle box clicks
 function handleBoxClick() {
-    
+    if (this.innerHTML !== "X" && this.innerHTML !== "O") {
 
+        const currentPlayer = X_or_O % 2 === 0 ? "X" : "O";
+
+        this.innerHTML = currentPlayer;
+        console.log(currentPlayer);
+
+        turn.innerHTML = (currentPlayer === "X" ? "O" : "X") + " Turn Now";
+        X_or_O += 1;
+
+        const winner = getWinner();
+        if (winner) {
+            X_or_O = 0; // Reset turn count
+            return;
+        }
+    }
 };
 
 // Function to reset the game
