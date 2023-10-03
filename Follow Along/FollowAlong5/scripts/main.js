117691f96956c6b80b9899f10cef5f3a1190e5ce
laritzdm <laritzdm@rose-hulman.edu>
2023-10-02 21:11:04 -0400

    Finished FA05 and fixed up replay function to fully text

--- Follow Along/FollowAlong5/scripts/main.js
+++ Follow Along/FollowAlong5/scripts/main.js
@@ -14,6 +14,12 @@ function main() {
 // Function to select and display the winner
 function selectWinnerBoxes(winnerBoxes) {
 
+    winnerBoxes.forEach((box) => {
+        box.classList.add("win");
+    });
+
+    turn.innerHTML = winnerBoxes[0].innerHTML + " is a winner";
+    turn.style.fontSize = "40px";
 };
 
 // Function to check and get the winner
@@ -29,11 +35,22 @@ function getWinner() {
         const boxA = boxes[a], boxB = boxes[b], boxC = boxes[c];
         console.log(a,b,c);
         console.log(boxA,boxB,boxC);
+        if (boxA.innerHTML !== "" && boxA.innerHTML === boxB.innerHTML &&
+                boxA.innerHTML === boxC.innerHTML) {
+
+            selectWinnerBoxes([boxA, boxB, boxC]);
+            return boxA.innerHTML;
+        }
     }
+    return null; // No winner yet
 };
 
 // Function to handle box clicks
 function handleBoxClick() {
+    if (getWinner()) {
+        // If there's already a winner, do nothing
+        return;
+    }
     if (this.innerHTML !== "X" && this.innerHTML !== "O") {
 
         const currentPlayer = X_or_O % 2 === 0 ? "X" : "O";
@@ -54,7 +71,14 @@ function handleBoxClick() {
 
 // Function to reset the game
 function replay() {
+    boxes.forEach((box) => {
+        box.classList.remove("win");
+        box.innerHTML = "";
+    })
 
+    X_or_O = 0; // Reset turn count
+    turn.innerHTML = "Play"; // Reset turn text
+    turn.style.fontSize = "25px"; // Reset turn text size
 };
 
 main();
