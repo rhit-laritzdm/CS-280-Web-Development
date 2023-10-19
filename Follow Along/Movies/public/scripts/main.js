6c629868bfea554b72bcca1f101e666b0acf0104
laritzdm <laritzdm@rose-hulman.edu>
2023-10-19 13:27:58 -0400

    uploaded initial movie files

---
+++ Follow Along/Movies/public/scripts/main.js
@@ -0,0 +1,7 @@
+var rhit = rhit || {};
+rhit.FB_COLLECTION_MOVIEQUOTE = "MovieQuotes";
+rhit.FB_KEY_QUOTE = "quote";
+rhit.FB_KEY_MOVIE = "movie";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.fbMovieQuotesManager = null;
+rhit.fbSingleQuoteManager = null;
