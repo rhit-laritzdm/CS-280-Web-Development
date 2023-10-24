05fba99baa6805bfadbf7c3f5de5a9353c9c28bb
laritzdm <laritzdm@rose-hulman.edu>
2023-10-24 00:39:39 -0400

    created chuck norris joke generator (scenario 4)

--- Follow Along/FA09 API/public/scripts/mainChuckNorris.js
+++ Follow Along/FA09 API/public/scripts/mainChuckNorris.js
@@ -1,28 +1,38 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const fetchDataButton = document.getElementById("fetchDataButton");
-    const dataIndexInput = document.getElementById("dataIndexInput");
-    const dataDisplay = document.getElementById("dataDisplay");
-    
-    fetchDataButton.addEventListener("click", function() {
-        const dataIndex = dataIndexInput.value;
-        if (dataIndex) {
-            fetch(`https://jsonplaceholder.typicode.com/todos/${dataIndex}`)
-                .then(response => response.json())
-                .then(data => {
-                    dataDisplay.innerHTML = `<strong>Title:</strong> ${data.title} <br> <strong>Completed:</strong> ${data.completed}`;
-                    $("#dataIndexModal").modal('hide');
-                })
-                .catch(error => {
-                    console.error("Error fetching data: ", error);
-                    dataDisplay.innerHTML = "Error fetching data. Please try again.";
-                })
-                .finally(() => {
-                    // Ensure modal and its backdrop are dismissed
-                    $("#dataIndexModal").modal('hide');
-                    $('body').removeClass('modal-open');
-                    $('.modal-backdrop').remove();
-                });
-        };
-    });
+    const dataTableHead = document.querySelector("#dataTable thead tr");
+    const dataTableBody = document.querySelector("#dataTable tbody tr");
 
+    fetchDataButton.addEventListener("click", function() {
+        fetch(`https://api.chucknorris.io/jokes/random`)
+            .then(response => response.json())
+            .then(data => {
+                // Clear previous headers and data
+                dataTableHead.innerHTML = '';
+                dataTableBody.innerHTML = '';
+
+                // Populate the table
+                // for (const key in data) {
+                //     dataTableHead.insertAdjacentHTML('beforeend', `<th>${key}</th>`);
+                //     dataTableBody.insertAdjacentHTML('beforeend', `<td>${data[key]}</td>`);
+                // }
+                dataTableHead.insertAdjacentHTML('beforeend', `<th>Source</th>`);
+                dataTableBody.insertAdjacentHTML('beforeend', `<td>${data["url"]}</td>`);
+                dataTableHead.insertAdjacentHTML('beforeend', `<th>Joke</th>`);
+                dataTableBody.insertAdjacentHTML('beforeend', `<td>${data["value"]}</td>`);
+                    
+                // Close the modal
+                $("#dataIndexModal").modal('hide');
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+                dataTableBody.innerHTML = `<td colspan="2">Error fetching data. Please try again.</td>`;
+            })
+            .finally(() => {
+                // Ensure modal and its backdrop are dismissed
+                $("#dataIndexModal").modal('hide');
+                $('body').removeClass('modal-open');
+                $('.modal-backdrop').remove();
+            });
+    });
 });
