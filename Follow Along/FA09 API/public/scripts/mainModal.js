fcf8750f6a2099c49567cf8cd420502a0b2be835
laritzdm <laritzdm@rose-hulman.edu>
2023-10-23 23:10:40 -0400

    Copied scenario 2 code to scenario 3

--- Follow Along/FA09 API/public/scripts/mainModal.js
+++ Follow Along/FA09 API/public/scripts/mainModal.js
@@ -25,33 +25,3 @@ fetchDataButton.addEventListener("click", function() {
             });
     };
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.getElementById('fetchData').addEventListener('click', function() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('dataContainer').innerHTML = `
-                <p><strong>Title:</strong> ${data.title}</p>
-                <p><strong>ID:</strong> ${data.id}</p>
-                <p><strong>Completed:</strong> ${data.completed}</p>
-            `;
-        })
-        .catch(error => {
-            console.error("There was an error fetching data:", error);
-        });
-});
---
+++ Follow Along/FA09 API/public/scripts/mainTable.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const fetchDataButton = document.getElementById("fetchDataButton");
+    const dataIndexInput = document.getElementById("dataIndexInput");
+    const dataDisplay = document.getElementById("dataDisplay");
+});
+
+fetchDataButton.addEventListener("click", function() {
+    const dataIndex = dataIndexInput.value;
+    if (dataIndex) {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${dataIndex}`)
+            .then(response => response.json())
+            .then(data => {
+                dataDisplay.innerHTML = `<strong>Title:</strong> ${data.title} <br> <strong>Completed:</strong> ${data.completed}`;
+                $("#dataIndexModal").modal('hide');
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+                dataDisplay.innerHTML = "Error fetching data. Please try again.";
+            })
+            .finally(() => {
+                // Ensure modal and its backdrop are dismissed
+                $("#dataIndexModal").modal('hide');
+                $('body').removeClass('modal-open');
+                $('modal-backdrop').remove();
+            });
+    };
+});
