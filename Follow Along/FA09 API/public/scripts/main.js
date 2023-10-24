210a8d7150acdd43c5a990d04820dc26fc9d5e07
laritzdm <laritzdm@rose-hulman.edu>
2023-10-23 23:07:31 -0400

    FA09 API Finished scenario 2

--- Follow Along/FA09 API/public/scripts/main.js
+++ Follow Along/FA09 API/public/scripts/main.js
@@ -1,5 +1,5 @@
 document.getElementById('fetchData').addEventListener('click', function() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    fetch(`https://jsonplaceholder.typicode.com/todos/1`)
         .then(response => response.json())
         .then(data => {
             document.getElementById('dataContainer').innerHTML = `
